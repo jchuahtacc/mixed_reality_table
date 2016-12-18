4b44476ac4cbca35a8bfc54e8fb62a070d9347a4
jchuahtacc <jchuah@tacc.utexas.edu>
2016-12-18 10:18:01 -0600

    Ported test_markers functionality to detect_markers

--- lib/mrtable/settings.hpp
+++ lib/mrtable/settings.hpp
@@ -35,6 +35,7 @@ namespace mrtable {
                 return false;
             fs << "camera_matrix" << camMatrix;
             fs << "distortion_coefficients" << distCoeffs;
+            fs.release();
             return true;
         }
 
@@ -89,6 +90,7 @@ namespace mrtable {
             fs << "maxErroneousBitsInBorderRate" << params.maxErroneousBitsInBorderRate;
             fs << "minOtsuStdDev" << params.minOtsuStdDev;
             fs << "errorCorrectionRate" << params.errorCorrectionRate;
+            fs.release();
             return true;
         }
     
@@ -117,138 +119,6 @@ namespace mrtable {
             }
             string contents = string(child_node->value());
             return stod(contents);
-        }
-
-        void readDetectorParameters(const char * filename, DetectorParameters* params) throw() {
-            // Load xml file contents
-            ifstream file;
-            file.exceptions( ifstream::failbit | ifstream::badbit );
-            file.open (filename);
-            string contents;
-            file.seekg(0, std::ios::end);
-            contents.reserve(file.tellg());
-            file.seekg(0, std::ios::beg);
-            contents.assign((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-
-            // Create XML document object
-            xml_document<> doc;
-            char* contents_buf = new char[(int)contents.length() + 1];
-            strcpy(contents_buf, contents.c_str());
-            doc.parse<0>(contents_buf);
-
-            // Validate format 
-            xml_node<char> *root = doc.first_node("DetectorParameters", 0, false);
-            if (!root) {
-                throw std::runtime_error("Missing DetectorParameters node");
-            }
-
-            // Get DetectorParameter settings
-            try {
-                params->adaptiveThreshConstant = getVal(root, "adaptiveThreshConstant");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->adaptiveThreshWinSizeMax  = (int)getVal(root, "adaptiveThreshWinSizeMax");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->adaptiveThreshWinSizeMin  = (int)getVal(root, "adaptiveThreshWinSizeMin");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->adaptiveThreshWinSizeStep  = (int)getVal(root, "adaptiveThreshWinSizeStep");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->cornerRefinementMaxIterations  = (int)getVal(root, "cornerRefinementMaxIterations");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->cornerRefinementMinAccuracy = getVal(root, "cornerRefinementMinAccuracy");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->cornerRefinementWinSize = (int)getVal(root, "cornerRefinementWinSize");
-            } catch (const std::runtime_error& er) {
-            } 
-            try {
-                params->markerBorderBits = (int)getVal(root, "markerBorderBits");
-            } catch (const std::runtime_error& er) {
-            } 
-            try {
-                params->maxErroneousBitsInBorderRate = getVal(root, "maxErroneousBitsInBorderRate");
-            } catch (const std::runtime_error& er) {
-            } 
-            try {
-                params->maxMarkerPerimeterRate = getVal(root, "maxMarkerPerimeterRate");
-            } catch (const std::runtime_error& er) {
-            } 
-            try {
-                params->minCornerDistanceRate = getVal(root, "minCornerDistanceRate");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->minDistanceToBorder = (int)getVal(root, "minDistanceToBorder");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->minMarkerDistanceRate = getVal(root, "minMarkerDistanceRate");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->minOtsuStdDev = getVal(root, "minOtsuStdDev");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->perspectiveRemoveIgnoredMarginPerCell = getVal(root, "perspectiveRemoveIgnoredMarginPerCell");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->perspectiveRemovePixelPerCell = (int)getVal(root, "perspectiveRemovePixelPerCell");
-            } catch (const std::runtime_error& er) {
-            }
-            try {
-                params->polygonalApproxAccuracyRate = getVal(root, "polygonalApproxAccuracyRate");
-            } catch (const std::runtime_error& er) {
-            }
-            xml_node< char > * cornerRefinementNode = root->first_node("doCornerRefinement");
-            if (cornerRefinementNode) {
-                char * contents = cornerRefinementNode->value();
-                if (strcasecmp(contents, "false") == 0) {
-                    params->doCornerRefinement = false;
-                }
-                if (strcasecmp(contents, "true") == 0) {
-                    params->doCornerRefinement = true;
-                }
-            }
-        }
-
-        void writeDetectorParameters(const char * filename, DetectorParameters params) {
-            ofstream file(filename);
-            file << "<?xml version=\"1.0\" standalone=\"yes\" ?>" << endl;
-            file << "<DetectorParameters>" << endl;
-            file << "<adaptiveThreshConstant>" << params.adaptiveThreshConstant << "</adaptiveThreshConstant>" << endl;
-            file << "<adaptiveThreshWinSizeMax>" << params.adaptiveThreshWinSizeMax << "</adaptiveThreshWinSizeMax>" << endl;
-            file << "<adaptiveThreshWinSizeMin>" << params.adaptiveThreshWinSizeMin << "</adaptiveThreshWinSizeMin>" << endl;
-            file << "<adaptiveThreshWinSizeStep>" << params.adaptiveThreshWinSizeStep << "</adaptiveThreshWinSizeStep>" << endl;
-            file << "<cornerRefinementMaxIterations>" << params.cornerRefinementMaxIterations << "</cornerRefinementMaxIterations>" << endl;
-            file << "<cornerRefinementMinAccuracy>" << params.cornerRefinementMinAccuracy << "</cornerRefinementMinAccuracy>" << endl;
-            file << "<cornerRefinementWinSize>" << params.cornerRefinementWinSize << "</cornerRefinementWinSize>" << endl;
-            file << "<errorCorrectionRate>" << params.errorCorrectionRate << "</errorCorrectionRate>" << endl;
-            file << "<markerBorderBits>" << params.markerBorderBits << "</markerBorderBits>" << endl;
-            file << "<maxErroneousBitsInBorderRate>" << params.maxErroneousBitsInBorderRate << "</maxErroneousBitsInBorderRate>" << endl;
-            file << "<maxMarkerPerimeterRate>" << params.maxMarkerPerimeterRate << "</maxMarkerPerimeterRate>" << endl;
-            file << "<minCornerDistanceRate>" << params.minCornerDistanceRate << "</minCornerDistanceRate>" << endl;
-            file << "<minDistanceToBorder>" << params.minDistanceToBorder << "</minDistanceToBorder>" << endl;
-            file << "<minMarkerDistanceRate>" << params.minMarkerDistanceRate << "</minMarkerDistanceRate>" << endl;
-            file << "<minMarkerPerimeterRate>" << params.minMarkerPerimeterRate << "</minMarkerPerimeterRate>" << endl;
-            file << "<minOtsuStdDev>" << params.minOtsuStdDev << "</minOtsuStdDev>" << endl;
-            file << "<perspectiveRemoveIgnoredMarginPerCell>" << params.perspectiveRemoveIgnoredMarginPerCell << "</perspectiveRemoveIgnoredMarginPerCell>" << endl;
-            file << "<perspectiveRemovePixelPerCell>" << params.perspectiveRemovePixelPerCell << "</perspectiveRemovePixelPerCell>" << endl;
-            file << "<polygonalApproxAccuracyRate>" << params.polygonalApproxAccuracyRate << "</polygonalApproxAccuracyRate>" << endl;
-            file << "<doCornerRefinement>" << (params.doCornerRefinement ? "true" : "false") << "</doCornerRefinement>" << endl;
-            file << "</DetectorParameters>" << endl;
-            file.close();
         }
 
         void printDetectorParameters(DetectorParameters params) {
--- src/detect_markers.cpp
+++ src/detect_markers.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 using namespace cv;
 using namespace mrtable::settings;
+using namespace std::chrono;
 
 namespace {
 const char* about = "Basic marker detection";
@@ -15,54 +16,91 @@ const char* keys  =
         "DICT_6X6_50=8, DICT_6X6_100=9, DICT_6X6_250=10, DICT_6X6_1000=11, DICT_7X7_50=12,"
         "DICT_7X7_100=13, DICT_7X7_250=14, DICT_7X7_1000=15, DICT_ARUCO_ORIGINAL = 16}"
         "{v        |       | Input from video file, if ommited, input comes from camera }"
+        "{i        |       | Input from a single image file }"
         "{ci       | 0     | Camera id if input doesnt come from video (-v) }"
         "{c        |       | Camera intrinsic parameters. Needed for camera pose }"
         "{l        | 0.1   | Marker side lenght (in meters). Needed for correct scale in camera pose }"
         "{dp       |       | File of marker detector parameters }"
+        "{op       |       | List of overridden detector parameters, i.e. adaptiveThreshConstant=5,doCornerRefinement=false}"
+        "{o        |       | Output camera and detector parameters and preview renderings. File prefix is output_}"
+        "{p        |       | Show preview in GUI window }"
+        "{verbose  |       | Verbose output of settings }"
         "{r        |       | show rejected candidates too }";
 }
 
-/**
-static bool readCameraParameters(string filename, Mat &camMatrix, Mat &distCoeffs) {
-    FileStorage fs(filename, FileStorage::READ);
-    if(!fs.isOpened())
-        return false;
-    fs["camera_matrix"] >> camMatrix;
-    fs["distortion_coefficients"] >> distCoeffs;
-    return true;
+typedef struct result_t {
+    int frames;
+    int detected;
+    int elapsed;
+} result_t;
+
+int dictionaryId;
+bool showRejected;
+bool estimatePose;
+bool preview;
+bool output;
+bool verbose;
+float markerLength;
+
+aruco::DetectorParameters detectorParams;
+aruco::Dictionary dictionary;
+Mat camMatrix, distCoeffs;
+vector< int > ids;
+vector< vector< Point2f > > corners, rejected;
+vector< Vec3d > rvecs, tvecs;
+
+void outputResults(result_t result) {
+    cout << "Frames processed: " << result.frames << endl;
+    cout << "Milliseconds elapsed: " << result.elapsed << endl;
+    cout << "Markers detected: " << result.detected << endl;
+    cout << "Average frames per second: " << (result.frames * 1000.0 / result.elapsed) << endl;
+    cout << "Average markers detected per frame: " << (result.detected * 1.0 / result.frames) << endl;
 }
-*/
 
-
-/**
-static bool readDetectorParameters(string filename, aruco::DetectorParameters *params) {
-    FileStorage fs(filename, FileStorage::READ);
-    if(!fs.isOpened())
-        return false;
-    fs["adaptiveThreshWinSizeMin"] >> params->adaptiveThreshWinSizeMin;
-    fs["adaptiveThreshWinSizeMax"] >> params->adaptiveThreshWinSizeMax;
-    fs["adaptiveThreshWinSizeStep"] >> params->adaptiveThreshWinSizeStep;
-    fs["adaptiveThreshConstant"] >> params->adaptiveThreshConstant;
-    fs["minMarkerPerimeterRate"] >> params->minMarkerPerimeterRate;
-    fs["maxMarkerPerimeterRate"] >> params->maxMarkerPerimeterRate;
-    fs["polygonalApproxAccuracyRate"] >> params->polygonalApproxAccuracyRate;
-    fs["minCornerDistanceRate"] >> params->minCornerDistanceRate;
-    fs["minDistanceToBorder"] >> params->minDistanceToBorder;
-    fs["minMarkerDistanceRate"] >> params->minMarkerDistanceRate;
-    fs["doCornerRefinement"] >> params->doCornerRefinement;
-    fs["cornerRefinementWinSize"] >> params->cornerRefinementWinSize;
-    fs["cornerRefinementMaxIterations"] >> params->cornerRefinementMaxIterations;
-    fs["cornerRefinementMinAccuracy"] >> params->cornerRefinementMinAccuracy;
-    fs["markerBorderBits"] >> params->markerBorderBits;
-    fs["perspectiveRemovePixelPerCell"] >> params->perspectiveRemovePixelPerCell;
-    fs["perspectiveRemoveIgnoredMarginPerCell"] >> params->perspectiveRemoveIgnoredMarginPerCell;
-    fs["maxErroneousBitsInBorderRate"] >> params->maxErroneousBitsInBorderRate;
-    fs["minOtsuStdDev"] >> params->minOtsuStdDev;
-    fs["errorCorrectionRate"] >> params->errorCorrectionRate;
-    return true;
+string getFilename(string filename) {
+    for (int i = filename.length(); i >= 0; i--) {
+        if (filename[i] == '/') {
+            return filename.substr(i + 1);
+        }
+    }
+    return filename;
 }
-*/
 
+result_t processImage(Mat image, Mat* imageCopy) {
+    steady_clock::time_point detectBegin = steady_clock::now();
+
+    // detect markers and estimate pose
+    aruco::detectMarkers(image, dictionary, corners, ids, detectorParams, rejected);
+    if(estimatePose && ids.size() > 0)
+        aruco::estimatePoseSingleMarkers(corners, markerLength, camMatrix, distCoeffs, rvecs,
+                                         tvecs);
+    steady_clock::time_point detectEnd = steady_clock::now();
+    long elapsed = duration_cast<milliseconds>(detectEnd - detectBegin).count();
+ 
+    // draw results
+    if (imageCopy) {
+        image.copyTo(*imageCopy);
+        if(ids.size() > 0) {
+            aruco::drawDetectedMarkers(*imageCopy, corners, ids);
+
+            if(estimatePose) {
+                for(unsigned int i = 0; i < ids.size(); i++)
+                    aruco::drawAxis(*imageCopy, camMatrix, distCoeffs, rvecs[i], tvecs[i],
+                                    markerLength * 0.5f);
+            }
+        }
+
+        if(showRejected && rejected.size() > 0)
+            aruco::drawDetectedMarkers(*imageCopy, rejected, noArray(), Scalar(100, 0, 255));
+    }
+
+    result_t result;
+    result.frames = 1;
+    result.elapsed = elapsed;
+    result.detected = ids.size();
+
+    return result;
+}
 
 /**
  */
@@ -75,12 +113,15 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    int dictionaryId = parser.get<int>("d");
-    bool showRejected = parser.has("r");
-    bool estimatePose = parser.has("c");
-    float markerLength = parser.get<float>("l");
+    dictionaryId = parser.get<int>("d");
+    showRejected = parser.has("r");
+    estimatePose = parser.has("c");
+    markerLength = parser.get<float>("l");
+    preview = parser.has("p");
+    output = parser.has("o");
+    verbose = parser.has("verbose");
+    String overrideParameters = parser.get<String>("op");
 
-    aruco::DetectorParameters detectorParams;
     if(parser.has("dp")) {
         bool readOk = readDetectorParameters(parser.get<string>("dp"), &detectorParams);
         if(!readOk) {
@@ -92,19 +133,13 @@ int main(int argc, char *argv[]) {
 
     int camId = parser.get<int>("ci");
 
-    String video;
-    if(parser.has("v")) {
-        video = parser.get<String>("v");
-    }
-
     if(!parser.check()) {
         parser.printErrors();
         return 0;
     }
 
-    aruco::Dictionary dictionary = getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
+    dictionary = getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
 
-    Mat camMatrix, distCoeffs;
     if(estimatePose) {
         bool readOk = readCameraParameters(parser.get<string>("c"), camMatrix, distCoeffs);
         if(!readOk) {
@@ -113,9 +148,48 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    if (!overrideParameters.empty()) {
+        parseDetectorParameters(overrideParameters.c_str(), &detectorParams);
+    }
+
+    if (verbose) {
+        cout << "Camera Matrix: " << endl << camMatrix << endl;
+        cout << "Distortion Coefficients: " << endl << distCoeffs << endl;
+        printDetectorParameters(detectorParams);
+    }
+
+    if(output) {
+        writeCameraParameters("output_cameraParams.xml", camMatrix, distCoeffs);
+        writeDetectorParameters("output_detectorParams.xml", detectorParams);
+    }
+
+    if (parser.has("i")) {
+        string filename = parser.get<String>("i");
+        Mat image, imageCopy;
+        image = imread(filename);
+        result_t result = processImage(image, &imageCopy);
+        if (output) {
+            imwrite("output_" + getFilename(filename), imageCopy); 
+        }
+        if (preview) {
+            cout << "Press the Escape key to continue..." << endl;
+            imshow("out", imageCopy);
+            char key = (char)waitKey(0);
+        }
+        outputResults(result);
+        return 0;
+    }
+
+    String video;
+    if(parser.has("v")) {
+        video = parser.get<String>("v");
+    }
+
     VideoCapture inputVideo;
     int waitTime;
+    bool videoFile = false;
     if(!video.empty()) {
+        videoFile = true;
         inputVideo.open(video);
         waitTime = 0;
     } else {
@@ -123,52 +197,68 @@ int main(int argc, char *argv[]) {
         waitTime = 10;
     }
 
-    double totalTime = 0;
-    int totalIterations = 0;
+    VideoWriter outputVideo;
+
+    // Disable camera capture -- can't get it working
+    if (!videoFile && output) {
+        output = false;
+    }
+    if (output) {
+        int ex;
+        Size S = Size((int) inputVideo.get(CV_CAP_PROP_FRAME_WIDTH), (int) inputVideo.get(CV_CAP_PROP_FRAME_HEIGHT));
+ 
+        string outputFilename;
+        if (!videoFile) {
+            ex = CV_FOURCC('H','2','6','3');
+            std::ostringstream oss;
+            oss << "camera_" << camId << ".avi";
+            outputFilename = "output_" + oss.str(); 
+        } else {
+            ex = static_cast<int>(inputVideo.get(CV_CAP_PROP_FOURCC));
+            outputFilename = "output_" + getFilename(video);
+        }
+        outputVideo.open(outputFilename, ex, inputVideo.get(CV_CAP_PROP_FPS), S, true);
+        if (!outputVideo.isOpened()) {
+            cout << "Unable to open video file for output: " << ("output_" + getFilename(video)) << endl;
+            return -1;
+        }
+    }
+
+    result_t aggregate;
+
+    if (preview) {
+        cout << "Press the Escape key to continue..." << endl;
+    }
+
 
     while(inputVideo.grab()) {
         Mat image, imageCopy;
         inputVideo.retrieve(image);
+        
+        result_t result;
 
-        double tick = (double)getTickCount();
-
-        vector< int > ids;
-        vector< vector< Point2f > > corners, rejected;
-        vector< Vec3d > rvecs, tvecs;
-
-        // detect markers and estimate pose
-        aruco::detectMarkers(image, dictionary, corners, ids, detectorParams, rejected);
-        if(estimatePose && ids.size() > 0)
-            aruco::estimatePoseSingleMarkers(corners, markerLength, camMatrix, distCoeffs, rvecs,
-                                             tvecs);
-
-        double currentTime = ((double)getTickCount() - tick) / getTickFrequency();
-        totalTime += currentTime;
-        totalIterations++;
-        if(totalIterations % 30 == 0) {
-            cout << "Detection Time = " << currentTime * 1000 << " ms "
-                 << "(Mean = " << 1000 * totalTime / double(totalIterations) << " ms)" << endl;
+        if (preview || output) {
+            result = processImage(image, &imageCopy);
+        } else {
+            result = processImage(image, NULL);
         }
 
-        // draw results
-        image.copyTo(imageCopy);
-        if(ids.size() > 0) {
-            aruco::drawDetectedMarkers(imageCopy, corners, ids);
+        aggregate.frames++;
+        aggregate.detected += result.detected;
+        aggregate.elapsed += result.elapsed;
 
-            if(estimatePose) {
-                for(unsigned int i = 0; i < ids.size(); i++)
-                    aruco::drawAxis(imageCopy, camMatrix, distCoeffs, rvecs[i], tvecs[i],
-                                    markerLength * 0.5f);
-            }
+        if (output) {
+            outputVideo << imageCopy;
         }
 
-        if(showRejected && rejected.size() > 0)
-            aruco::drawDetectedMarkers(imageCopy, rejected, noArray(), Scalar(100, 0, 255));
-
-        imshow("out", imageCopy);
-        char key = (char)waitKey(waitTime);
-        if(key == 27) break;
+        if (preview) {
+            imshow("out", imageCopy);
+            char key = (char)waitKey(waitTime);
+            if(key == 27) break;
+        }
     }
+
+    outputResults(aggregate);
 
     return 0;
 }
