622fc9eb46a0b34eda30bb431871ad1e0ec3b6cd
jchuahtacc <jchuah@tacc.utexas.edu>
2016-12-20 18:38:14 -0600

    SimpleBlobDetector::Params utility functions

--- lib/mrtable/settings.hpp
+++ lib/mrtable/settings.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <opencv2/aruco.hpp>
 #include <opencv2/core/core.hpp>
+#include <opencv2/features2d.hpp>
 #include <fstream>
 #include <streambuf>
 #include <string>
@@ -33,6 +34,58 @@ namespace mrtable {
                 return false;
             fs << "camera_matrix" << camMatrix;
             fs << "distortion_coefficients" << distCoeffs;
+            fs.release();
+            return true;
+        }
+
+        bool readBlobParameters(string filename, Ptr<SimpleBlobDetector::Params> &params) {
+            FileStorage fs(filename, FileStorage::READ);
+            if (!fs.isOpened()) 
+                return false;
+            fs["blobColor"] >> params->blobColor;
+            fs["filterByArea"] >> params->filterByArea;
+            fs["filterByCircularity"] >> params->filterByCircularity;
+            fs["filterByColor"] >> params->filterByColor;
+            fs["filterByConvexity"] >> params->filterByConvexity;
+            fs["filterByInertia"] >> params->filterByInertia;
+            fs["maxArea"] >> params->maxArea;
+            fs["maxCircularity"] >> params->maxCircularity;
+            fs["maxInertiaRatio"] >> params->maxInertiaRatio;
+            fs["maxThreshold"] >> params->maxThreshold;
+            fs["minArea"] >> params->minArea;
+            fs["minCircularity"] >> params->minCircularity;
+            fs["minInertiaRatio"] >> params->minInertiaRatio;
+            fs["minThreshold"] >> params->minThreshold;
+            fs["minDistBetweenBlobs"] >> params->minDistBetweenBlobs;
+            int temp;
+            fs["minRepeatability"] >> temp;
+            params->minRepeatability = temp;
+            fs["thresholdStep"] >> params->thresholdStep;
+            return true;
+        }
+
+        bool writeBlobParameters(string filename, Ptr<SimpleBlobDetector::Params> params) {
+            FileStorage fs(filename, FileStorage::WRITE);
+            if(!fs.isOpened())
+                return false;
+            fs << "blobColor" << params->blobColor;
+            fs << "filterByArea" << params->filterByArea;
+            fs << "filterByCircularity" << params->filterByCircularity;
+            fs << "filterByColor" << params->filterByColor;
+            fs << "filterByConvexity" << params->filterByConvexity;
+            fs << "filterByInertia" << params->filterByInertia;
+            fs << "maxArea" << params->maxArea;
+            fs << "maxCircularity" << params->maxCircularity;
+            fs << "maxInertiaRatio" << params->maxInertiaRatio;
+            fs << "maxThreshold" << params->maxThreshold;
+            fs << "minArea" << params->minArea;
+            fs << "minCircularity" << params->minCircularity;
+            fs << "minInertiaRatio" << params->minInertiaRatio;
+            fs << "minThreshold" << params->minThreshold;
+            fs << "minDistBetweenBlobs" << params->minDistBetweenBlobs;
+            int temp = params->minRepeatability;
+            fs << "minRepeatability" << temp; 
+            fs << "thresholdStep" << params->thresholdStep;
             fs.release();
             return true;
         }
@@ -91,7 +144,29 @@ namespace mrtable {
             fs.release();
             return true;
         }
-    
+
+        void printBlobParameters(Ptr<SimpleBlobDetector::Params> params) {
+            cout << "blobColor: " << (unsigned int)params->blobColor << endl;
+            cout << "filterByArea: " << (params->filterByArea ? "true" : "false") << endl;
+            cout << "filterByCircularity: " << (params->filterByCircularity ? "true" : "false") << endl;
+            cout << "filterByColor: " << (params->filterByColor ? "true" : "false") << endl;
+            cout << "filterByConvexity: " << (params->filterByConvexity ? "true" : "false") << endl;
+            cout << "filterByInertia: " << (params->filterByInertia ? "true" : "false") << endl;
+            cout << "maxArea: " << params->maxArea << endl;
+            cout << "maxCircularity: " << params->maxCircularity << endl;
+            cout << "maxConvexity: " << params->maxConvexity << endl;
+            cout << "maxInertiaRatio: " << params->maxInertiaRatio << endl;
+            cout << "maxThreshold: " << params->maxThreshold << endl;
+            cout << "minArea: " << params->minArea << endl;
+            cout << "minCircularity: " << params->minCircularity << endl;
+            cout << "minConvexity: " << params->minConvexity << endl;
+            cout << "minDistBetweenBlobs: " << params->minDistBetweenBlobs << endl;
+            cout << "minInertiaRatio: " << params->minInertiaRatio << endl;
+            cout << "minRepeatability: " << params->minRepeatability << endl;
+            cout << "minThreshold: " << params->minThreshold << endl;
+            cout << "thresholdStep: " << params->thresholdStep << endl;
+        }
+   
        void printDetectorParameters(Ptr<DetectorParameters> params) {
             cout << "adaptiveThreshConstant: " << params->adaptiveThreshConstant << endl;
             cout << "adaptiveThreshWinSizeMax: " << params->adaptiveThreshWinSizeMax << endl;
@@ -113,6 +188,97 @@ namespace mrtable {
             cout << "perspectiveRemovePixelPerCell: " << params->perspectiveRemovePixelPerCell << endl;
             cout << "polygonalApproxAccuracyRate: " << params->polygonalApproxAccuracyRate << endl;
             cout << "doCornerRefinement: " << (params->doCornerRefinement ? "true" : "false") << endl;
+        }
+
+        void parseBlobParameters(const char * paramString, Ptr<SimpleBlobDetector::Params> &params) {
+            string input = paramString;
+            istringstream ss(input);
+            string token;
+            while (std::getline(ss, token, ',')) {
+                istringstream pair(token);
+                string key, value;
+                if (std::getline(pair, key, '=') && std::getline(pair, value)) {
+                    transform(key.begin(), key.end(), key.begin(), ::tolower);
+                    transform(value.begin(), value.end(), value.begin(), ::tolower);
+                    if (key.compare("blobcolor") == 0) {
+                        params->blobColor = stoi(value);
+                    }
+                    if (key.compare("filterbyarea") == 0) {
+                        if (value.compare("true") == 0) {
+                            params->filterByArea = true;
+                        }
+                        if (value.compare("false") == 0) {
+                            params->filterByArea = false;
+                        }
+                    }
+                    if (key.compare("filterbycolor") == 0) {
+                        if (value.compare("true") == 0) {
+                            params->filterByColor = true;
+                        }
+                        if (value.compare("false") == 0) {
+                            params->filterByColor = false;
+                        }
+                    }
+                    if (key.compare("filterbyconvexity") == 0) {
+                        if (value.compare("true") == 0) {
+                            params->filterByConvexity = true;
+                        }
+                        if (value.compare("false") == 0) {
+                            params->filterByConvexity = false;
+                        }
+                    }
+                    if (key.compare("filterbyinertia") == 0) {
+                         if (value.compare("true") == 0) {
+                            params->filterByInertia = true;
+                        }
+                        if (value.compare("false") == 0) {
+                            params->filterByInertia = false;
+                        }
+                    }
+                    if (key.compare("maxarea") == 0) {
+                        params->maxArea = stof(value);
+                    }
+                    if (key.compare("maxcircularity") == 0) {
+                        params->maxCircularity = stoi(value);
+                    }
+                    if (key.compare("maxconvexity") == 0) {
+                        params->maxConvexity = stof(value);
+                    }
+                    if (key.compare("maxinertiaRatio") == 0) {
+                        params->maxInertiaRatio = stoi(value);
+                    }
+                    if (key.compare("maxthreshold") == 0) {
+                        params->maxThreshold = stof(value);
+                    }
+                    if (key.compare("minarea") == 0) {
+                        params->minArea = stof(value);
+                    }
+                    if (key.compare("mincircularity") == 0) {
+                        params->minCircularity = stof(value);
+                    }
+                    if (key.compare("minconvexity") == 0) {
+                        params->minConvexity = stof(value);
+                    }
+                    if (key.compare("mindistbetweenblobs") == 0) {
+                        params->minDistBetweenBlobs = stoi(value);
+                    }
+                    if (key.compare("mininertiaratio") == 0) {
+                        params->minInertiaRatio = stof(value);
+                    }
+                    if (key.compare("minrepeatability") == 0) {
+                        params->minRepeatability = stoi(value);
+                    }
+                    if (key.compare("minthreshold") == 0) {
+                        params->minInertiaRatio = stof(value);
+                    }
+                    if (key.compare("thresholdstep") == 0) {
+                        params->minInertiaRatio = stof(value);
+                    }
+                    if (key.compare("mininertiaratio") == 0) {
+                        params->minInertiaRatio = stof(value);
+                    }
+                }                
+            }
         }
 
         void parseDetectorParameters(const char * paramString, Ptr<DetectorParameters> &params) {
