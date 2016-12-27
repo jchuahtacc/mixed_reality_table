f7cc69059b640224384f8b9c606f3e447200ec48
jchuahtacc <jchuah@tacc.utexas.edu>
2016-12-26 23:46:33 -0600

    verbose output

--- src/detect_server.cpp
+++ src/detect_server.cpp
@@ -13,8 +13,8 @@ const char* about = "Basic marker detection";
 const char* keys  =
         "{h        |       | Print this help message}"
         "{v        |       | Input from video file (to play on a loop), if ommited, input comes from camera }"
-        "{c        |       | Server configuration XML file }"
-        "{o        |       | Output configuration XML files }"
+        "{c        |       | Server configuration XML file (otherwise, defaults will be used)}"
+        "{o        |       | Output configuration XML (serverConfig.xml, cameraParameters.xml, etc..)}"
         "{ci       | 0     | Camera id if input doesnt come from video (-v) }"
         "{p        |       | Show preview in GUI window }"
         "{verbose  |       | Verbose output of settings }"
@@ -23,6 +23,7 @@ const char* keys  =
 
 cv::Ptr<ServerConfig> config;
 string configFile = "serverConfig.xml";
+bool verbose = false;
 
 int main(int argc, char** argv) {
     CommandLineParser parser(argc, argv, keys);
@@ -34,6 +35,12 @@ int main(int argc, char** argv) {
     }
 
     config = ServerConfig::create(parser.get<String>("c"));
+
+    verbose = parser.has("verbose");
+
+    if (verbose) {
+        cout << config;
+    }
 
     if (parser.has("o")) {
         config->write("serverConfig.xml");
