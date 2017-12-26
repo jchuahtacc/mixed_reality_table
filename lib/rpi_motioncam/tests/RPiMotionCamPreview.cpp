99ec8ef5301c09b502b86820695f7c3935054e2c
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2017-12-26 21:09:58 +0000

    Delegated camera management to libraspivid

--- lib/rpi_motioncam/tests/RPiMotionCamPreview.cpp
+++ lib/rpi_motioncam/tests/RPiMotionCamPreview.cpp
@@ -1,9 +1,43 @@
-#include "RPiMotionCam.h"
 #include <iostream>
-#include "interface/mmal/mmal_types.h"
+#include "RPiMotionCam.h"
+#include <chrono>
+#include <thread>
 
 using namespace std;
 using namespace rpi_motioncam;
+
+bool running = true;
+
+void consume_frames() {
+    cout << "Frame consumer thread started" << endl;
+    auto last_time = std::chrono::system_clock::now();
+    int bytes = 0;
+    int frames = 0;
+    int regions = 0;
+    int seconds = 0;
+    while (running) {
+        if (MotionData::has_ready_frame()) {
+            while (MotionData::has_ready_frame()) {
+                frames++;
+                shared_ptr< MotionData > frame;
+                MotionData::get_ready_frame(frame);
+                regions += frame->regions->size();
+                for (auto region = frame->regions->begin(); region != frame->regions->end(); ++region) {
+                    bytes += region->imgPtr->total();
+                }
+                if (std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::system_clock::now() - last_time ).count() > 1000) {
+                    seconds++;
+                    last_time = std::chrono::system_clock::now();
+                    cout << bytes << " bytes consumed from " << regions << " regions across " << frames << " frames" << endl;
+                    bytes = 0;
+                    frames = 0;
+                    regions = 0;
+                }
+            }
+        }
+    }
+    cout << "Frame consumer thread ending" << endl;
+}
 
 int wait() {
     sigset_t waitset;
@@ -19,19 +53,37 @@ int wait() {
 
 
 int main(int argc, char** argv) {
-    RPiMotionCam* cam = RPiMotionCam::create();
-    MMAL_STATUS_T status = cam->init(PICAM_DEFAULT, true);
-    if (status == MMAL_SUCCESS) {
+    RPI_MOTIONCAM_OPTION_S options = RPiMotionCam::createMotionCamDefaultOptions();
+    options.preview = true;
+    auto cam = RPiMotionCam::create(options);
+
+    if (cam->init() == MMAL_SUCCESS) {
         cout << "Init success" << endl;
     } else {
-        cout << "Failure with error " << status << endl;
+        cout << "Init failed" << endl;
         return -1;
     }
-    status = cam->start();
-    if (status != MMAL_SUCCESS) {
+
+    if (cam->start() == MMAL_SUCCESS) {
+        cout << "Start success" << endl;
+    } else {
         cout << "Start failed" << endl;
         return -1;
     }
 
+    std::thread frame_consumer_thread(consume_frames);
+
     while (wait());
+
+    cout << "User interrupt" << endl;
+
+    running = false;
+
+    frame_consumer_thread.join();
+
+    cout << "Frame consumer thread stopped" << endl;
+
+    cam->stop();
+
+    cout << "Camera stopped" << endl;
 }
