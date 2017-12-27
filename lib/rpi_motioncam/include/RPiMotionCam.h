b2cc1588169001a2b0bbafc05eda0dab67493546
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2017-12-27 01:47:49 +0000

    Revert "Delegated camera management to libraspivid"
    This reverts commit 99ec8ef5301c09b502b86820695f7c3935054e2c.

--- lib/rpi_motioncam/include/RPiMotionCam.h
+++ lib/rpi_motioncam/include/RPiMotionCam.h
@@ -1,100 +1,112 @@
-#ifndef _GNU_SOURCE
-   #define _GNU_SOURCE
-#endif
+#ifndef __RPIMOTIONCAM_HPP__
+#define __RPIMOTIONCAM_HPP__
 
-#include <memory>
+#include "bcm_host.h"
+#include "interface/vcos/vcos.h"
+#include "interface/mmal/mmal.h"
+#include "interface/mmal/mmal_types.h"
+#include "interface/mmal/mmal_logging.h"
+#include "interface/mmal/mmal_buffer.h"
+#include "interface/mmal/util/mmal_util.h"
+#include "interface/mmal/util/mmal_util_params.h"
+#include "interface/mmal/util/mmal_default_components.h"
+#include "interface/mmal/util/mmal_connection.h"
 
-#include "RaspiVid.h"
-#include "MotionVectorCallback.h"
-#include "MotionVectorPreviewCallback.h"
-#include "RawOutputCallback.h"
+// Camera capture modes (resolutions and frame rates)
+// https://www.raspberrypi.org/documentation/raspbian/applications/camera.md
+#define     PICAM_DEFAULT           0
+#define     PICAM1X_1920_1080_30    1
+#define     PICAM1X_2592_1944_30    2
+#define     PICAM1X_1296_972_42     3
+#define     PICAM1X_1296_730_49     4
+#define     PICAM1X_640_480_60      5
+#define     PICAM1X_640_480_90      6
+#define     PICAM2X_1920_1080_30    7
+#define     PICAM2X_3280_2464_15    8
+#define     PICAM2X_1640_1232_40    9
+#define     PICAM2X_1640_922_40     10
+#define     PICAM2X_1280_720_90     11
+#define     PICAM2X_640_480_90      12
+#define     PICAM1X                 1
+#define     PICAM2X                 2
 
-// Video format information
-// 0 implies variable
-#define VIDEO_FRAME_RATE_NUM 30
-#define VIDEO_FRAME_RATE_DEN 1
-
-/// Video render needs at least 2 buffers.
-#define VIDEO_OUTPUT_BUFFERS_NUM 3
+#define     VIDEO_OUTPUT_BUFFERS_NUM        3
+#define     PREVIEW_LAYER                   2
+#define     MAX_BITRATE_LEVEL_4             25000000
 
 namespace rpi_motioncam {
-    using namespace raspivid;
-    using namespace raspi_cam_control;
-
-    // Max bitrate we allow for recording
-    const int MAX_BITRATE_MJPEG = 25000000; // 25Mbits/s
-    const int MAX_BITRATE_LEVEL4 = 25000000; // 25Mbits/s
-    const int MAX_BITRATE_LEVEL42 = 62500000; // 62.5Mbits/s
-
-    /// Interval at which we check for an failure abort during capture
-    const int ABORT_INTERVAL = 100; // ms
-
-    int mmal_status_to_int(MMAL_STATUS_T status);
-    static void signal_handler(int signal_number);
-
-    // Forward
-    typedef struct RASPIVID_STATE_S RASPIVID_STATE;
-
-    /** Possible raw output formats
-     */
-    typedef enum {
-       RAW_OUTPUT_FMT_YUV = 1,
-       RAW_OUTPUT_FMT_RGB,
-       RAW_OUTPUT_FMT_GRAY,
-    } RAW_OUTPUT_FMT;
-
-    /** Structure containing all state information for the current run
-     */
-    struct RPI_MOTIONCAM_OPTION_S
-    {
-       uint32_t width;                          /// Requested width of image
-       uint32_t height;                         /// requested height of image
-       uint32_t framerate;                      /// Requested frame rate (fps)
-       bool verbose;                        /// !0 if want detailed run information
-
-       RAW_OUTPUT_FMT raw_output_fmt;       /// The raw video format
-       int cameraNum;                       /// Camera number
-       int sensor_mode;			            /// Sensor mode. 0=auto. Check docs/forum for modes selected by other values.
-
-       uint32_t resizer_width;               /// Resizer component width for compressed motion vectors
-       uint32_t resizer_height;              /// Resizer component height 
-       
-       bool preview;
-
-    };
-
     class RPiMotionCam {
+        
         public:
-            static shared_ptr< RPiMotionCam > create();
-            static shared_ptr< RPiMotionCam > create(RPI_MOTIONCAM_OPTION_S options);
-            static shared_ptr< RPiMotionCam > getInstance();
-            static RPI_MOTIONCAM_OPTION_S createMotionCamDefaultOptions();
+            static RPiMotionCam* create();
             MMAL_STATUS_T init();
+            MMAL_STATUS_T init(int mode);
+            MMAL_STATUS_T init(int mode, bool preview);
             MMAL_STATUS_T start();
-            void stop();
-            ~RPiMotionCam();
-
-        protected:
-            RPI_MOTIONCAM_OPTION_S options_;
+            int getWidth();
+            int getHeight();
+            int getFPS();
 
         private:
             RPiMotionCam();
-            static shared_ptr< RPiMotionCam > singleton_;
+            ~RPiMotionCam();
+            static RPiMotionCam *instance_;
 
-            MMAL_STATUS_T create_components();
-            MMAL_STATUS_T connect_components();
+            uint32_t width_;
+            uint32_t height_;
+            uint32_t fps_;
+            uint32_t camera_mode_;
+            bool preview_ = false;
+            MMAL_COMPONENT_T *camera_component;
+            MMAL_COMPONENT_T *splitter_component;
+            MMAL_COMPONENT_T *encoder_component;
+            MMAL_COMPONENT_T *preview_component;
+            MMAL_COMPONENT_T *null_sink_component;
+
+            MMAL_PORT_T *camera_preview_port = NULL;
+            MMAL_PORT_T *camera_video_port = NULL;
+            MMAL_PORT_T *camera_still_port = NULL;
+            MMAL_PORT_T *preview_input_port = NULL;
+            MMAL_PORT_T *encoder_input_port = NULL;
+            MMAL_PORT_T *encoder_output_port = NULL;
+            MMAL_PORT_T *splitter_input_port = NULL;
+            MMAL_PORT_T *splitter_output_port_0 = NULL;
+            MMAL_PORT_T *splitter_output_port_1 = NULL;
+            MMAL_PORT_T *splitter_preview_port = NULL;
+            MMAL_PORT_T *null_sink_input_port = NULL;
+            MMAL_CONNECTION_T *preview_connection = NULL;
+            MMAL_CONNECTION_T *splitter_connection = NULL;
+            MMAL_CONNECTION_T *encoder_connection = NULL;
+            MMAL_POOL_T *encoder_pool = NULL;
+            MMAL_POOL_T *splitter_pool = NULL;
+
+            MMAL_ES_FORMAT_T *format;
+            MMAL_VIDEO_LEVEL_T h264level_;
+
+            MMAL_STATUS_T create_camera_component();
+            MMAL_STATUS_T create_preview_component();
+            MMAL_STATUS_T create_splitter_component();
+            MMAL_STATUS_T create_encoder_component();
+            MMAL_STATUS_T create_null_sink_component();
+            MMAL_STATUS_T make_connections();
             MMAL_STATUS_T add_callbacks();
+            MMAL_STATUS_T send_buffers();
 
-            shared_ptr< RaspiCamera > camera;
-            shared_ptr< RaspiRenderer > preview_renderer;
-            shared_ptr< RaspiEncoder > encoder;
-            shared_ptr< RaspiSplitter > splitter;
-            shared_ptr< RaspiResize > resizer;
-            shared_ptr< RaspiNullsink > preview_nullsink;
-            shared_ptr< RaspiNullsink > still_nullsink;
+            MMAL_STATUS_T connect_ports(MMAL_PORT_T *output_port, MMAL_PORT_T *input_port, MMAL_CONNECTION_T **connection);
 
-            shared_ptr< MotionVectorCallback > mvCallback;
-            shared_ptr< RawOutputCallback > roCallback;
+            void check_disable_port(MMAL_PORT_T *port);
+            void destroy_camera_ports();
+            void destroy_connections();
+            void disable_components();
+            void destroy_components();
 
-        };
+            bool setCameraMode(int mode);
+
+            static void camera_control_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer);
+            static void encoder_buffer_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer);
+            static void splitter_buffer_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer);
+
+    };
 }
+
+#endif
--- lib/rpi_motioncam/tests/RPiMotionCamPreview.cpp
+++ lib/rpi_motioncam/tests/RPiMotionCamPreview.cpp
@@ -1,43 +1,9 @@
-#include <iostream>
 #include "RPiMotionCam.h"
-#include <chrono>
-#include <thread>
+#include <iostream>
+#include "interface/mmal/mmal_types.h"
 
 using namespace std;
 using namespace rpi_motioncam;
-
-bool running = true;
-
-void consume_frames() {
-    cout << "Frame consumer thread started" << endl;
-    auto last_time = std::chrono::system_clock::now();
-    int bytes = 0;
-    int frames = 0;
-    int regions = 0;
-    int seconds = 0;
-    while (running) {
-        if (MotionData::has_ready_frame()) {
-            while (MotionData::has_ready_frame()) {
-                frames++;
-                shared_ptr< MotionData > frame;
-                MotionData::get_ready_frame(frame);
-                regions += frame->regions->size();
-                for (auto region = frame->regions->begin(); region != frame->regions->end(); ++region) {
-                    bytes += region->imgPtr->total();
-                }
-                if (std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::system_clock::now() - last_time ).count() > 1000) {
-                    seconds++;
-                    last_time = std::chrono::system_clock::now();
-                    cout << bytes << " bytes consumed from " << regions << " regions across " << frames << " frames" << endl;
-                    bytes = 0;
-                    frames = 0;
-                    regions = 0;
-                }
-            }
-        }
-    }
-    cout << "Frame consumer thread ending" << endl;
-}
 
 int wait() {
     sigset_t waitset;
@@ -53,37 +19,19 @@ int wait() {
 
 
 int main(int argc, char** argv) {
-    RPI_MOTIONCAM_OPTION_S options = RPiMotionCam::createMotionCamDefaultOptions();
-    options.preview = true;
-    auto cam = RPiMotionCam::create(options);
-
-    if (cam->init() == MMAL_SUCCESS) {
+    RPiMotionCam* cam = RPiMotionCam::create();
+    MMAL_STATUS_T status = cam->init(PICAM_DEFAULT, true);
+    if (status == MMAL_SUCCESS) {
         cout << "Init success" << endl;
     } else {
-        cout << "Init failed" << endl;
+        cout << "Failure with error " << status << endl;
         return -1;
     }
-
-    if (cam->start() == MMAL_SUCCESS) {
-        cout << "Start success" << endl;
-    } else {
+    status = cam->start();
+    if (status != MMAL_SUCCESS) {
         cout << "Start failed" << endl;
         return -1;
     }
 
-    std::thread frame_consumer_thread(consume_frames);
-
     while (wait());
-
-    cout << "User interrupt" << endl;
-
-    running = false;
-
-    frame_consumer_thread.join();
-
-    cout << "Frame consumer thread stopped" << endl;
-
-    cam->stop();
-
-    cout << "Camera stopped" << endl;
 }
