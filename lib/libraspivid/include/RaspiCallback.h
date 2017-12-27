dcbec12db5d271a513f3f321e28bfd8a75685938
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2017-12-27 01:47:49 +0000

    Revert "Added Doxygen comments"
    This reverts commit 90234bfd758a3fd7c82a96658636de4810890871.

--- lib/libraspivid/include/RaspiCallback.h
+++ lib/libraspivid/include/RaspiCallback.h
@@ -1,7 +1,3 @@
-/**
- \file RaspiCallback.h
- */
-
 #ifndef __RASPICALLBACK_H__
 #define __RASPICALLBACK_H__
 
@@ -19,26 +15,9 @@
 
 
 namespace raspivid {
-    /**
-     \class RaspiCallback "RaspiCallback.h"
-     \brief A wrapper class for implementing port callbacks
-     */
     class RaspiCallback {
         public:
-            /**
-             \brief Callback function for buffer data. The buffer is automatically locked, released and returned by RaspiPort.
-             \param port A C pointer to the MMAL_PORT_T where the callback is originating from.
-             \param buffer A C pointer to the MMAL_BUFFER_HEADER_T containing buffer data.
-             \see RaspiPort::add_callback
-             */
             virtual void callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) =0;
-            
-            /**
-             \brief A callback function, called after the main callback function. This allows implementing classes to perform operations after the
-             buffer has been released back to the port. This prevents the port from being starved of buffers due to long post processing operations.
-             \see RaspiPort::add_callback
-             \see RaspiCallback::callback
-             */
             virtual void post_process() { };
     };
 }
--- lib/libraspivid/include/components/RaspiComponent.h
+++ lib/libraspivid/include/components/RaspiComponent.h
@@ -1,6 +1,3 @@
-/**
- \file components/RaspiComponent.h
- */
 #ifndef __RASPICOMPONENT_H__
 #define __RASPICOMPONENT_H__ 
 
@@ -25,43 +22,14 @@ using namespace std;
 
 namespace raspivid {
 
-    /**
-     \class RaspiComponent RaspiComponent.h "components/RaspiComponent.h"
-     \brief An abstract base class for all Raspberry Pi MMAL components.
-     */
     class RaspiComponent {
         public:
-            /**
-             \brief Class destructor.
-             */
             ~RaspiComponent();
-
-            /**
-             \brief Shuts down and cleans up component.
-             */
             void destroy();
-
-            /**
-             \brief Connects another component's default_output to this component's default_input.
-             \param source_component The component with a default_output port supplying frames to this component's default_input port
-             \return An MMAL_STATUS_T result. MMAL_SUCCESS_T if successful, MMAL_EINVAL if the supplied component has no default_output port or if this
-             component has no default_input port
-             \see #default_input
-             \see #default_output
-             */
             MMAL_STATUS_T connect(shared_ptr< RaspiComponent > source_component);
-
-            /**
-             \brief Connects RaspiPort to this component's default_input.
-             \param source_port The port supplying frames to this component's default_input port
-             \return An MMAL_STATUS_T result. MMAL_SUCCESS_T if successful, MMAL_EINVAL if this component has no default_input port.
-             \see RaspiComponent::connect
-             \see #default_input
-             \see #default_output
-             */
             MMAL_STATUS_T connect(shared_ptr< RaspiPort > source_port);
-            shared_ptr< RaspiPort > default_input = nullptr;    /**< Default input port for this component */
-            shared_ptr< RaspiPort > default_output = nullptr;   /**< Default output port for this component */
+            shared_ptr< RaspiPort > default_input = nullptr;
+            shared_ptr< RaspiPort > default_output = nullptr;
         protected:
             RaspiComponent();
             MMAL_STATUS_T init();
